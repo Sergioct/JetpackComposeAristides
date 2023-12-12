050d74241f53a0fafa842b9cffc16b1e60843c68
Sergio Crespo Toubes <sergiocrespotoubes@gmail.com>
2023-12-12 08:29:57 +0100

    Update gradle

--- app/src/main/java/com/sergiocrespotoubes/jetpackcomposearistides/ui/screen/section2/Exercise2Screen.kt
+++ app/src/main/java/com/sergiocrespotoubes/jetpackcomposearistides/ui/screen/section2/Exercise2Screen.kt
@@ -2,72 +2,51 @@ package com.sergiocrespotoubes.jetpackcomposearistides.ui.screen.section2
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 
 @Preview(showBackground = true)
 @Composable
-fun Exercise1Screen() {
-    Column {
-        Box(
-            Modifier
-                .background(Color.Cyan)
-                .fillMaxWidth()
-                .weight(1f)
-        ) {
-            Text(
-                text = "Ejemplo 1",
-                Modifier.align(Alignment.Center)
-            )
-        }
-        Row(
-            Modifier
-                .background(Color.Cyan)
-                .fillMaxWidth()
-                .weight(1f)
-        ) {
-            Box(
-                Modifier
-                    .background(Color.Red)
-                    .fillMaxHeight()
-                    .weight(1f)
-            ) {
-                Text(
-                    text = "Ejemplo 2",
-                    Modifier.align(Alignment.Center)
-                )
-            }
-            Box(
-                Modifier
-                    .background(Color.Green)
-                    .fillMaxHeight()
-                    .weight(1f)
-            ) {
-                Text(
-                    text = "Ejemplo 3",
-                    Modifier.align(Alignment.Center)
-                )
-            }
-        }
-        Box(
-            Modifier
-                .background(Color.Magenta)
-                .fillMaxWidth()
-                .weight(1f)
-        ) {
-            Text(
-                text = "Ejemplo 4",
-                Modifier.align(Alignment.BottomCenter)
-            )
-        }
+fun Exercise2Screen() {
+    ConstraintLayout(modifier = Modifier.fillMaxSize()){
+        val (boxRed, boxBlue, boxYellow, boxMagenta, boxGreen) = createRefs()
+
+        Box(modifier = Modifier.size(125.dp).background(Color.Red).constrainAs(boxRed){
+            top.linkTo(parent.top)
+            bottom.linkTo(parent.bottom)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        })
+        Box(modifier = Modifier.size(125.dp).background(Color.Green).constrainAs(boxGreen){
+            top.linkTo(boxRed.bottom)
+            end.linkTo(boxRed.start)
+        })
+        Box(modifier = Modifier.size(125.dp).background(Color.Yellow).constrainAs(boxYellow){
+            bottom.linkTo(boxRed.top)
+            end.linkTo(boxRed.start)
+        })
+        Box(modifier = Modifier.size(125.dp).background(Color.Magenta).constrainAs(boxMagenta){
+            bottom.linkTo(boxRed.top)
+            start.linkTo(boxRed.end)
+        })
+        Box(modifier = Modifier.size(125.dp).background(Color.Blue).constrainAs(boxBlue){
+            top.linkTo(boxRed.bottom)
+            start.linkTo(boxRed.end)
+        })
     }
 }
